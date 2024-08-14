438fedb194b27b9d7248b24db2faf17493d53b3e
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com>
2024-08-14 23:16:40 +0200

    improve
    improve dotfiles setup, add initial install script
    still in progress

---
+++ install_packages.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# define paths
+DOTFILES_DIR=$(dirname "$(realpath "$0")")
+
+# function to check if a command exists
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+# update and install essential packages
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm zsh openssh stow unzip man-db base-devel tmux neovim keychain bat tree
+
+# create the zshenv file in /etc/zsh/ using a heredoc
+sudo tee /etc/zsh/zshenv > /dev/null << 'EOF'
+if [[ ! -o norcs ]]; then
+  export ZDOTDIR="$HOME/.config/zsh"
+fi
+EOF
+
+# source the zshenv file
+echo "sourcing /etc/zsh/zshenv..."
+. /etc/zsh/zshenv
+
+# change the user's shell to Zsh if it's not already Zsh
+if [ "$SHELL" != "/bin/zsh" ]; then
+    echo "changing the default shell to zsh for the current user..."
+    chsh -s /bin/zsh
+    echo "default shell changed to zsh. please log out and log back in for the change to take effect."
+fi
+
+# ensure ZDOTDIR exists
+if [ ! -d "$ZDOTDIR" ]; then
+    echo "creating $ZDOTDIR..."
+    mkdir -p "$ZDOTDIR"
+fi
+
+# install oh-my-zsh
+if [ ! -d "$ZDOTDIR/oh-my-zsh" ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git $ZDOTDIR/oh-my-zsh
+fi
+
+# ensure ~/.local/bin exists
+if [ ! -d ~/.local/bin ]; then
+    echo "creating ~/.local/bin directory..."
+    mkdir -p ~/.local/bin
+fi
+
+# install oh-my-posh
+if [ ! -f ~/.local/bin/oh-my-posh ]; then
+    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+fi
+
+echo "packages installed."
+
---
+++ setup_dotfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# define paths
+DOTFILES_DIR=$(dirname "$(realpath "$0")")
+CONFIG_DIR="$HOME/.config"
+
+# function to check if a command exists
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+# check if stow is installed
+if ! command_exists stow; then
+    echo "stow is not installed. please install it and try again"
+    exit 1
+fi
+
+# create necessary directories inside .config based on dotfiles repo structure
+echo "creating necessary directories inside .config..."
+for dir in "$DOTFILES_DIR"/.config/*; do
+    if [ -d "$dir" ]; then
+        mkdir -p "$CONFIG_DIR/$(basename "$dir")"
+    fi
+done
+
+# create plugins directory for tmux config (to avoid symlink the directory)
+mkdir -p "$CONFIG_DIR/tmux/plugins"
+
+# use stow to manage the config directory
+echo "stowing config..."
+stow -d "$DOTFILES_DIR" -t "$HOME" .
+
+# clone the kickstart.nvim repository for neovim configuration
+echo "cloning kickstart.nvim..."
+git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
+echo "dotfiles have been set up."
